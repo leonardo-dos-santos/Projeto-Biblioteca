6d16bedab8484e0b70c094b814337d141aff9848
Leonardo <129320373+leonardo-dos-santos@users.noreply.github.com>
2024-05-07 18:55:33 -0300

    criação do constructor do livro

---
+++ js/script.js
@@ -0,0 +1,13 @@
+class Book {
+  constructor(
+    title = 'Unknown',
+    author = 'Unknown',
+    pages = '0',
+    isRead = false
+  ) {
+    this.title = title
+    this.author = author
+    this.pages = pages
+    this.isRead = isRead
+  }
+}
