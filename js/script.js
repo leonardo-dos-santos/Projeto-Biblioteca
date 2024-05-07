a0b2005241012b6642385ab5be539e7ffad6119c
Leonardo <129320373+leonardo-dos-santos@users.noreply.github.com>
2024-05-07 18:56:02 -0300

    criação do constructor da biblioteca

--- js/script.js
+++ js/script.js
@@ -11,3 +11,29 @@ class Book {
     this.isRead = isRead
   }
 }
+
+class Library {
+  constructor() {
+    this.books = []
+  }
+
+  addBook(newBook) {
+    if (!this.isInLibrary(newBook)) {
+      this.books.push(newBook)
+    }
+  }
+
+  removeBook(title) {
+    this.books = this.books.filter((book) => book.title !== title)
+  }
+
+  getBook(title) {
+    return this.books.find((book) => book.title === title)
+  }
+
+  isInLibrary(newBook) {
+    return this.books.some((book) => book.title === newBook.title)
+  }
+}
+
+const library = new Library()
